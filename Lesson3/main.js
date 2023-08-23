10ed28590933a70e0f159003834302d95c63228c
Yevhenii <eugeneiv1@gmail.com>
2023-08-23 04:56:47 +0300

    Add improvement

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -46,13 +46,11 @@
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
             // let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-            //
+            // document.write('<ul>');
             // for (let item of listOfItems){
-            //     document.write(`<ul>
-            //                       <li>${item}</li>
-            //                     </ul>`);
+            //     document.write(`<li>${item}</li>`);
             // }
-
+            // document.write('</ul>');
 //
 // -----------------------------------------------
 //
