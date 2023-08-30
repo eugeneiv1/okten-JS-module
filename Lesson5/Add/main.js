cbb335378f229c4411b1a59dbeae5e305108ea64
Yevhenii <eugeneiv1@gmail.com>
2023-08-30 19:26:04 +0300

    Add additional for lesson 5

---
+++ Lesson5/Add/main.js
@@ -0,0 +1,62 @@
+// Все стірлочними!!!!!!!!!
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+
+            // let myArray = [2, 3, 7, 13, 754, 4]
+            // const calcAverage = arr => {
+            //     let sum = 0;
+            //     for (let number of arr) {
+            //         sum += number;
+            //     }
+            //     return sum / arr.length;
+            // }
+            //
+            // console.log(calcAverage(myArray))
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+
+            // const findMin = (...numbers) => {
+            //     let max = 0;
+            //     let min = 0;
+            //     for (let number of numbers) {
+            //         if (min > number) {
+            //             min = number;
+            //         }
+            //         if (max < number) {
+            //             max = number;
+            //         }
+            //     }
+            //     console.log(numbers);
+            //     console.log(max);
+            //     return min;
+            // }
+            //
+            // console.log(findMin(2, 4, 7467, 1, 436, -3));
+
+// - створити функцію яка заповнює масив рандомними числами
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+            // const fillArray = length => {
+            //     let myArray =[];
+            //     for (let i = 0; i < length; i++) {
+            //         myArray.push(Math.round(Math.random()*100));
+            //     }
+            //     return myArray
+            // }
+            //
+            // console.log(fillArray(10));
+
+
+
+// - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+            // let myArray = [2, 3, 7, 13, 754, 4]
+            // const reverse = arr => {
+            //     let reversedArray = [];
+            //     for (let i = arr.length - 1; i >= 0; i--) {
+            //         reversedArray.push(arr[i]);
+            //     }
+            //     return reversedArray
+            // }
+            //
+            // console.log(reverse(myArray));
